04bbe2b12ec4d6437b3e4e186d408086e03a2065
ashraymp <mpashray@gmail.com>
2018-03-06 05:40:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,46 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var Page_one = {
+    title:'ALERT!',
+    Heading : 'Change of class',
+    section : ' 4B ',
+    subject  : 'Java ',
+    date : 'Today',
+    memo : '<p>Today,Java class will be held in room number 132!Submit assignments.Today,Java class will be held in room number 132!Submit assignments</p><p>Today,Java class will be held in room number 132!Submit assignmentsToday,Java class will be held in room number 132!Submit assignmentsToday,Java class will be held in room number 132!Submit assignmentsToday,Java class will be held in room number 132!Submit assignmentsToday,Java class will be held in room number 132!Submit assignments</p><p>Today,Java class will be held in room number 132!Submit assignmentsToday,Java class will be held in room number 132!Submit assignmentsToday,Java class will be held in room number 132!Submit assignmentsToday,Java class will be held in room number 132!Submit assignmentsToday,Java class will be held in room number 132!Submit assignmentsToday,Java class will be held in room number 132!Submit assignments</p>'
+};
+function create_template(data){
+    var date=data.date,title=data.title,Heading=data.Heading,section=data.section,subject=data.subject,memo=data.memo;
+    
+    
+    var templatehtml=
+                `<html>
+                <head>
+                    <title>${title}</title>
+                    <meta name="mfriendly" content="width-device-width,initial-scale=1"/>
+                            <link href="/ui/style.css" rel="stylesheet" />
+            
+                </head>
+                <body>
+                    <div>${Heading}</div>
+                    <div>${date}</div>
+                     <div>${section}</div>
+                      <div>${subject}</div>
+                     <div>${memo}</div>        
+                  
+                </body>
+            </html>`;
+
+    return templatehtml;
+}
+
+
+    
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
